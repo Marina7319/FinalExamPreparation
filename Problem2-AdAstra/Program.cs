122c01a0ef07644f51f88ad1d19252c5f0ec82ef
Marina7319 <marina-markova@abv.bg>
2022-09-04 12:07:14 +0300

    Commit Problem2-AdAstra. Commit Problem3-NeedForSpeed|||. Modify Problem3-HeroesOfCodeAndLogic7. Modify ExamPrep.sln file.

---
+++ Problem2-AdAstra/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AdAstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[#|](?<itemName>[A-z\s]+)*[#|](?<day>\d{2})/(?<month>\d{2})/(?<year>\d{2})*[#|](?<calories>\d+)*";
+            string input = Console.ReadLine();
+            Regex matches = new Regex(pattern);
+            MatchCollection productList = Regex.Matches(input, pattern);
+            double sumDays = 0;
+            foreach (Match match in productList)
+            {
+                var calories = match.Groups["calories"].Value;
+                double sumCalories = 0;
+                sumCalories += double.Parse(calories);
+                double days = sumCalories / 2000;
+                sumDays += days;
+            }
+            double sumFood = Math.Floor(sumDays);
+            Console.WriteLine($"You have food to last you for: {sumFood} days!");
+            if (sumFood == 0)
+            {
+
+            }
+            else
+            {
+                foreach (Match match in productList)
+                {
+                    var itemName = match.Groups["itemName"].Value;
+                    var day = match.Groups["day"].Value;
+                    var month = match.Groups["month"].Value;
+                    var year = match.Groups["year"].Value;
+                    var calories = match.Groups["calories"].Value;
+                    if (int.Parse(day) <= 31 && int.Parse(month) <= 12 && int.Parse(calories) <= 10000 && int.Parse(day) > 0 && int.Parse(month) > 0)
+                    {
+                        Console.WriteLine($"Item: {itemName}, Best before: {day}/{month}/{year}, Nutrition: {calories}");
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Problem3-NeedforSpeedIII/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NeedforSpeedIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            var cars = new Dictionary<string, KeyValuePair<int, int>>();
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] carsMileageFuel = Console.ReadLine().Split("|");
+                string car = carsMileageFuel[0];
+                int mileage = int.Parse(carsMileageFuel[1]);
+                int fuel = int.Parse(carsMileageFuel[2]);
+                if (!cars.ContainsKey(car))
+                {
+                    cars.Add(car, new KeyValuePair<int, int>(mileage, fuel));
+                }
+            }
+            string input = Console.ReadLine();
+            while (input != "Stop")
+            {
+                string[] commands = input.Split(" : ");
+                string command = commands[0];
+                switch (command)
+                {
+                    case "Drive":
+                        int distance = int.Parse(commands[2]);
+                        string car = commands[1];
+                        int fuel = int.Parse(commands[3]);
+                        int forOneMileage = distance / fuel;
+                        if (cars.ContainsKey(car))
+                        {
+                            if (cars[car].Value >= fuel)
+                            {
+                                int currDistance = distance + cars[car].Key;
+                                int currFuel = cars[car].Value - fuel;
+                                cars.Remove(car);
+                                cars.Add(car, new KeyValuePair<int, int>(currDistance, currFuel));
+                                Console.WriteLine($"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Not enough fuel to make that ride");
+                            }
+                            if(cars[car].Key >= 100000)
+                            {
+                                cars.Remove(car);
+                                Console.WriteLine($"Time to sell the {car}!");
+                            }
+                        }
+                        break;
+                    case "Refuel":
+                        string currCar = commands[1];
+                        int refillFuel = int.Parse(commands[2]);
+                        int distanceOfCar = cars[currCar].Key;
+                        int sumFuel = refillFuel + cars[currCar].Value;
+                        int sumRefillFuel = 0;
+                        if(sumFuel > 75)
+                        {
+                            sumFuel = 75;
+                            sumRefillFuel = 75 - cars[currCar].Value;
+                        }
+                        else
+                        {
+                            sumRefillFuel = refillFuel;
+                        }
+                        Console.WriteLine($"{currCar} refueled with {sumRefillFuel} liters");
+                        if (cars.ContainsKey(currCar))
+                        {
+                            cars.Remove(currCar);
+                            cars.Add(currCar, new KeyValuePair<int, int>(distanceOfCar, sumFuel));
+                        }
+                        break;
+                    case "Revert":
+                        string givenCar = commands[1];
+                        int givenKilometers = int.Parse(commands[2]);
+                        if (cars.ContainsKey(givenCar))
+                        {
+                            int currKilometers = cars[givenCar].Key - givenKilometers;
+                            int currFuelAmount = cars[givenCar].Value;
+                            cars.Remove(givenCar);
+                            if (currKilometers > 10000)
+                            {
+                                cars.Add(givenCar, new KeyValuePair<int, int>(currKilometers, currFuelAmount));
+                                Console.WriteLine($"{givenCar} mileage decreased by {givenKilometers} kilometers");
+                            }
+                            else
+                            {
+                                cars.Add(givenCar, new KeyValuePair<int, int>(10000, currFuelAmount));
+                            }
+                        }
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Key} -> Mileage: {car.Value.Key} kms, Fuel in the tank: {car.Value.Value} lt.");
+            }
+        }
+    }
+}
